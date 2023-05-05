1311f604843c1d67ccfc6926a1cc32e9fe584243
Pedro <pedrohalmeidamendonca@gmail.com>
2023-05-05 19:51:53 -0300

    Requisito 07

---
+++ src/Character.ts
@@ -0,0 +1,93 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+
+  public get race(): Race {
+    return this._race;
+  }
+
+  public get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public get defense(): number {
+    return this._defense;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public get energy(): Energy | undefined {
+    return this._energy ? { ...this._energy } : undefined;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public receiveDamage(attackPoints: number): number {
+    this._lifePoints -= Math.max(attackPoints - this.defense, 1);
+    return Math.max(this._lifePoints, -1);
+  }
+
+  public attack(enemy: Fighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    this._maxLifePoints = Math
+      .min(this._maxLifePoints, this.race.maxLifePoints);
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  special(): void {
+    this._strength *= 2;
+    this._defense *= 2;
+    this._maxLifePoints *= 2;
+    this._lifePoints *= 2;
+  }
+}
+
+export default Character;
